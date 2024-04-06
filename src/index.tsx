deac7df82f7c5b46b907d0ee5de7e6cba579861b
SigmaTheFox <valerio.donghia@outlook.com>
2024-04-06 11:12:28 +0200

    custom material design typedoc theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import { cpSync } from "node:fs";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
+import { cpSync } from 'node:fs';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-import { Application, JSX, RendererEvent, ParameterType } from "typedoc";
+import { Application, JSX, RendererEvent, ParameterType } from 'typedoc';
 
-import { getThemeCSSProperties } from "./theme.js";
+import { getThemeCSSProperties } from './theme.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -12,33 +12,40 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
  * Called by TypeDoc when loaded as a plugin.
  */
 export function load(app: Application) {
-  app.options.addDeclaration({
-    name: "themeColor",
-    help: "Material Theme: Material 3 source color to derive the theme from.",
-    type: ParameterType.String,
-    defaultValue: "#cb9820",
-  });
+	app.options.addDeclaration({
+		name: 'themeColor',
+		help: 'Material Theme: Material 3 source color to derive the theme from.',
+		type: ParameterType.String,
+		defaultValue: '#cb9820',
+	});
 
-  app.renderer.hooks.on("head.end", (event) => (
-    <>
-      <style>
-        <JSX.Raw
-          html={getThemeCSSProperties(
-            app.options.getValue("themeColor") as string,
-          )}
-        />
-      </style>
-      <link
-        rel="stylesheet"
-        href={event.relativeURL("assets/material-style.css")}
-      />
-    </>
-  ));
-  app.renderer.hooks.on("body.end", (event) => (
-    <script>
-      <JSX.Raw
-        html={
-          /* js */ `
+	app.renderer.hooks.on('head.begin', event => (
+		<>
+			<script>
+				<JSX.Raw
+					html={`window.va = window.va || function () {(window.vaq = window.vaq || []).push(arguments)};`}
+				/>
+			</script>
+			<script defer src="/_vercel/insights/script.js"></script>
+		</>
+	));
+
+	app.renderer.hooks.on('head.end', event => (
+		<>
+			<style>
+				<JSX.Raw
+					html={getThemeCSSProperties(app.options.getValue('themeColor') as string)}
+				/>
+			</style>
+			<link rel="stylesheet" href={event.relativeURL('assets/material-style.css')} />
+		</>
+	));
+	app.renderer.hooks.on('body.end', event => (
+		<>
+			<script>
+				<JSX.Raw
+					html={
+						/* js */ `
           try {
             const generateLinkElement = document.querySelector(".tsd-generator a");
             const link = document.createElement("a");
@@ -54,17 +61,15 @@ export function load(app: Application) {
 
           }
         `
-        }
-      />
-    </script>
-  ));
+					}
+				/>
+			</script>
+		</>
+	));
 
-  app.listenTo(app.renderer, RendererEvent.END, () => {
-    const from = resolve(__dirname, "../assets/style.css");
-    const to = resolve(
-      app.options.getValue("out"),
-      "assets/material-style.css",
-    );
-    cpSync(from, to);
-  });
+	app.listenTo(app.renderer, RendererEvent.END, () => {
+		const from = resolve(__dirname, '../assets/style.css');
+		const to = resolve(app.options.getValue('out'), 'assets/material-style.css');
+		cpSync(from, to);
+	});
 }
